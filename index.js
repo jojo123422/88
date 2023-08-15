b18ec1144dd03bcad4746e4ccedbe50b2399fd89
jumanah <jumanah.f.14@gmail.com>
2023-08-15 21:57:57 +0300

    add

---
+++ index.js
@@ -0,0 +1,19 @@
+// First exercise
+let name =prompt("الاسم")
+let age =prompt("العمر")
+
+if (age>=20)
+{
+    alert("مرحبا"+name)}
+else {
+alert("عذرا"+name)
+}
+// Second exercise
+let Name = prompt("ادخل الاسم")
+
+if (Name == "jumanah") {
+    alert("متطابق")
+}
+else{
+    alert("غير متطابق")
+}
